67fab39c260151a974dc992d48c231c0f28178be
hooktheduck <78820907+hooktheduck@users.noreply.github.com>
2022-04-28 14:05:06 +0200

    Add files via upload

--- lostark_asta_live.py
+++ lostark_asta_live.py
@@ -2,6 +2,7 @@ import dotenv
 import requests
 import os
 import discord
+import pickle
 
 from dotenv import load_dotenv
 from discord.ext import commands, tasks
@@ -13,50 +14,66 @@ discordName = os.getenv("DISCORD_NAME")
 channelId = os.getenv("CHANNEL_ID")
 bot = commands.Bot(command_prefix='!')
 
+def load():
+    with open('data.txt') as f:
+        return f.read()
+
+def write(text):
+    with open("data.txt", "w") as text_file:
+        text_file.write(text)
 
 def getAstaStatus():
+
+    # Namen für Offline-Status
+    offlineList = ["Maintenance", "Offline"]
+
+    # Namen für Online-Status
+    onlineList = ["Full", "Busy", "Online", "Good", "Ok"]
 
     try:
         response = requests.get("https://lastarkapi-m2.herokuapp.com/server/all").json()['data']['Asta'].split(" ")[1]
     except Exception:
         response = requests.get("https://lost-ark-api.vercel.app/server/all").json()['data']['Asta'].split(" ")[1]
         
-    if response == "Maintenance" or response == "Offline":
+    if response in offlineList:
         return "Offline"
 
-    if response == "Full" or response == "Busy" or response == "Online" or response == "Good":
+    if response in onlineList:
         return "Online"
 
 
 async def update():
-    if os.getenv("astaStatus") != getAstaStatus():
-        if os.getenv("astaStatus") == 'Offline' and getAstaStatus() == "Online":
+    if load() != getAstaStatus():
+        if load() == 'Offline' and getAstaStatus() == "Online":
             channel = bot.get_channel(int(channelId))
             await channel.send("Der Server **Asta** ist wieder online!")
             print("Server wieder online!")
-            dotenv.set_key(dotenv_file, "astaStatus", getAstaStatus())
+            write(getAstaStatus())
 
-        if os.getenv("astaStatus") == "Online" and getAstaStatus() == 'Offline':
+        if load() == "Online" and getAstaStatus() == 'Offline':
             channel = bot.get_channel(int(channelId))
             await channel.send("Der Server **Asta** ist jetzt offline!")
             print("Server ist offline!")
-            dotenv.set_key(dotenv_file, "astaStatus", getAstaStatus())
+            write(getAstaStatus())
 
 
 @bot.event
 async def on_ready():
-    dotenv_file = dotenv.find_dotenv()
-    dotenv.set_key(dotenv_file, "astaStatus", "Online")
+    write(getAstaStatus())
     await updateTimer.start()
 
 
 
 @tasks.loop(seconds=int(60))
 async def updateTimer():
-    await update()
-
+    print("Updated [every 60s]")
+    try:
+        await update()
+    except Exception as e:
+        print(e)
 
 bot.run(discordBotToken)
+
 
 
 
